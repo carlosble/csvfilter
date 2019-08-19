abc530cfbb5da6bf74773da836a88925bf2d4e5e
Carlos Ble <carlos@codesai.com>
2019-08-19 16:02:11 +0100

    green: tax_fields_are_mutually_exclusive

--- src/main/kotlin/csvFilter/CsvFilter.kt
+++ src/main/kotlin/csvFilter/CsvFilter.kt
@@ -2,7 +2,14 @@ package csvFilter
 
 class CsvFilter {
     fun filter(lines: List<String>) : List<String> {
-        return lines
+        val result = mutableListOf<String>()
+        result.add(lines[0])
+        val invoice = lines[1]
+        val fields = invoice.split(',')
+        if (fields[4].isNullOrEmpty() || fields[5].isNullOrEmpty()){
+            result.add(lines[1])
+        }
+        return result.toList()
     }
 }
 
--- src/test/kotlin/csvFilter/CsvFilterShould.kt
+++ src/test/kotlin/csvFilter/CsvFilterShould.kt
@@ -6,13 +6,22 @@ class CsvFilterShould {
     @Test
     fun correct_lines_are_not_filtered(){
         val headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente"
-        val invoiceLine = "1,02/05/2019,1000,810,19,ACER Laptop,B76430134,"
+        val invoiceLine = "1,02/05/2019,1000,810,19,,ACER Laptop,B76430134,"
         val result = CsvFilter().filter(
                 listOf(headerLine, invoiceLine))
         assertThat(result).isEqualTo(
                 listOf(headerLine, invoiceLine))
     }
 
+    @Test
+    fun tax_fields_are_mutually_exclusive(){
+        val headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente"
+        val invoiceLine = "1,02/05/2019,1000,810,19,8,ACER Laptop,B76430134,"
+        val result = CsvFilter().filter(
+            listOf(headerLine, invoiceLine))
+        assertThat(result).isEqualTo(
+            listOf(headerLine))
+    }
     /*@Test
     fun ignores_empty_or_null_lists(){
         assertThat(CsvFilter().filter(listOf()))
