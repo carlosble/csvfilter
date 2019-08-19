0e3ee5f62a0fa3bc50933b9be45ef9513b93bbe0
Carlos Ble <carlos@codesai.com>
2019-08-19 15:25:04 +0100

    initial commit with first red test

---
+++ src/main/kotlin/csvFilter/CsvFilter.kt
@@ -0,0 +1,8 @@
+package csvFilter
+
+class CsvFilter {
+    fun filter(lines: List<String>) : List<String> {
+        return listOf();
+    }
+}
+
---
+++ src/test/kotlin/csvFilter/CsvFilterShould.kt
@@ -0,0 +1,21 @@
+package csvFilter
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+class CsvFilterShould {
+
+    @Test
+    fun correct_lines_are_not_filtered(){
+        val headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente"
+        val invoiceLine = "1,02/05/2019,1000,810,19,ACER Laptop,B76430134,"
+        val result = CsvFilter().filter(
+                listOf(headerLine, invoiceLine))
+        assertThat(result).isEqualTo(
+                listOf(headerLine, invoiceLine));
+    }
+
+    /*@Test
+    fun ignores_empty_or_null_lists(){
+        assertThat(CsvFilter().filter(listOf()))
+                .isEqualTo(listOf<String>());
+    }*/
+}
