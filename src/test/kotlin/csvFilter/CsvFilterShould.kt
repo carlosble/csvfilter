39355805268028f416d7971e35819b0b5e6aae4b
carlosble <ble.jurado@gmail.com>
2019-08-28 16:16:50 +0100

    refactor: renames

--- src/test/kotlin/csvFilter/CsvFilterShould.kt
+++ src/test/kotlin/csvFilter/CsvFilterShould.kt
@@ -5,7 +5,7 @@ import org.junit.Test
 class CsvFilterShould {
     private val headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente"
     lateinit var filter : CsvFilter
-    private val emptyFile = listOf(headerLine)
+    private val emptyDataFile = listOf(headerLine)
     private val emptyField = ""
 
     @Before
@@ -15,41 +15,41 @@ class CsvFilterShould {
 
     @Test
     fun correct_lines_are_not_filtered(){
-        val invoiceLine = aFileWithLine(concept = "a correct line with irrelevant data")
-        val result = filter.apply(invoiceLine)
+        val lines = fileWithOneInvoiceLineHaving(concept = "a correct line with irrelevant data")
+        val result = filter.apply(lines)
 
-        assertThat(result).isEqualTo(invoiceLine)
+        assertThat(result).isEqualTo(lines)
     }
 
     @Test
     fun tax_fields_are_mutually_exclusive(){
-        val result = filter.apply(aFileWithLine(ivaTax = "19", igicTax = "8"))
+        val result = filter.apply(fileWithOneInvoiceLineHaving(ivaTax = "19", igicTax = "8"))
 
-        assertThat(result).isEqualTo(emptyFile)
+        assertThat(result).isEqualTo(emptyDataFile)
     }
 
     @Test
     fun there_must_be_at_least_one_tax_for_the_invoice(){
-        val result = filter.apply(aFileWithLine(ivaTax = emptyField, igicTax = emptyField))
+        val result = filter.apply(fileWithOneInvoiceLineHaving(ivaTax = emptyField, igicTax = emptyField))
 
-        assertThat(result).isEqualTo(emptyFile)
+        assertThat(result).isEqualTo(emptyDataFile)
     }
 
     @Test
     fun tax_fields_must_be_decimals(){
-        val result = filter.apply(aFileWithLine(ivaTax = "XYZ", igicTax = emptyField))
+        val result = filter.apply(fileWithOneInvoiceLineHaving(ivaTax = "XYZ", igicTax = emptyField))
 
-        assertThat(result).isEqualTo(emptyFile)
+        assertThat(result).isEqualTo(emptyDataFile)
     }
 
     @Test
     fun tax_fields_must_be_decimals_and_exclusive(){
-        val result = filter.apply(aFileWithLine(ivaTax = "XYZ", igicTax = "12"))
+        val result = filter.apply(fileWithOneInvoiceLineHaving(ivaTax = "XYZ", igicTax = "12"))
 
-        assertThat(result).isEqualTo(emptyFile)
+        assertThat(result).isEqualTo(emptyDataFile)
     }
 
-    private fun aFileWithLine(ivaTax: String = "19", igicTax: String = emptyField, concept: String = "irrelevant"): List<String> {
+    private fun fileWithOneInvoiceLineHaving(ivaTax: String = "19", igicTax: String = emptyField, concept: String = "irrelevant"): List<String> {
         val invoiceId = "1"
         val invoiceDate = "02/05/2019"
         val grossAmount = "1000"
